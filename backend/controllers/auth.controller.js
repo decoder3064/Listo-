de2ab3fe946754a473970642ae616489e16e8998
David R. <dreyes@colgate.edu>
2025-10-18 16:35:44 -0400

    authentication implemented

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcryptjs";
+import {prisma} from "../utils/prisma";
+import { generateToken } from "../utils/generateToken";
+
+
+export const registerUser = async (req, res) => {
+    const {name, email, password, confirmPassword} = req.body 
+    
+    try {
+
+         if (!name || !email || !password || !confirmPassword) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({message: "Invalid email format"});
+        }
+
+        const nameRegex = /^[a-zA-Z\s-]{2,50}$/;
+        if (!nameRegex.test(name)) {
+            return res.status(400).json({message: "Name must be 2-50 characters and contain only letters"});
+        }
+
+         if (password.length < 8) {
+            return res.status(400).json({message: "Password must be at least 8 characters"});
+        }
+
+          if (password !== confirmPassword) {
+            return res.status(400).json({message: "Passwords do not match"});
+        }
+
+        const userExists = await prisma.user.findUnique({where: { email }});
+        if (userExists){
+            return res.status(400).json({message: "User already exists"});
+        }
+ 
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({ data: {name, email, password: hashedPassword}});
+        const token = generateToken(user.id);
+        const {password: _, ... userWithoutPassword} = user
+        res.status(201).json({ ...userWithoutPassword, token});
+    }catch (error){
+        res.status(500).json({message:"Server error"});
+    }
+};
+
+
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+
+        if (!email || !password) {
+            return res.status(400).json({message: "Email and password are required"});
+        }
+
+        const user = await prisma.user.findUnique({where: { email }}); 
+
+        if (user && (await bcrypt.compare(password, user.password ))){
+            const token = generateToken(user.id);
+            const {password: _, ... userWithoutPassword} = user
+            res.json({...userWithoutPassword, token})
+        }else {
+            res.status(401).json({message: "Invalid email or password"})};
+    }
+    catch(error){
+        res.status(500).json({message: "Server error"});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 //Middleware 
 app.use(express.json()); 
 app.use(cors());
 
+app.use("/api/auth", authRoutes);
 
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
