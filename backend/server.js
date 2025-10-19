fae73d632279999fde9760cc98c384c63bb59ac3
David R. <dreyes@colgate.edu>
2025-10-18 20:48:17 -0400

    fixed bugs, all routes working

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/task.routes.js";  
+import taskRoutes from "./routes/tasks.routes.js";  
 
 
 dotenv.config();
