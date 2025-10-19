2bcaac7cb492e491f64a592342f6ba2dc5cc8a98
David R. <dreyes@colgate.edu>
2025-10-19 15:30:06 -0400

    added api service

---
+++ frontend/frontend/src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+
+const API_URL = 'http://localhost:3000/api'
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {'Content-Type': 'application/json',}
+});
+
+api.interceptors.request.use((config)=> {
+    const token = localStorage.getItem('token');
+    if (token){
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const authAPI = {
+    register: (userData) => api.post('/auth/register', userData),
+    login: (credentials) => api.post('/auth/login', credentials)
+
+};
+
+export const taskAPI = {
+    createTask:(taskData) => api.post('/tasks', taskData),
+    getTasks:() => api.get('/tasks'), 
+    updateTask:(id, taskData) => api.put(`/tasks/${id}`, taskData),
+    deleteTask:(id) => api.delete(`/tasks/${id}`),
+};
+
+export default api;
+
+
